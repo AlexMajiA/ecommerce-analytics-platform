e629a9c87133a2227dd518ffdbe8ab635a0b52a6
Alejandro Martínez Jiménez <amj.parrales@gmail.com>
2026-01-19 19:49:25 +0000

    Add v1 staging models for orders and customers

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,9 @@
+{{ 
+    config(
+    materialized='view', alias='mv_stg_customers'
+    ) 
+}}
+
 with source as (
 
     select *
@@ -10,9 +16,9 @@ renamed as (
     select
         customer_id,
         customer_unique_id,
-        customer_zip_code_prefix,
-        customer_city,
-        customer_state,
+        cast(customer_zip_code_prefix as INTEGER)   as customer_zip_code_prefix,
+        trim(customer_city)                         as customer_city,
+        trim(customer_state)                        as customer_state
     from source
 
 )
