d3114ef4f3e7d87432c050c01a3bf2aa14416b50
Alejandro Martínez Jiménez <amj.parrales@gmail.com>
2026-03-11 18:31:50 +0000

    dimension customer ok

--- models/marts/core/sales/dim_customers.sql
+++ models/marts/core/sales/dim_customers.sql
@@ -1,12 +1,11 @@
 {{
     config(
         materialized = 'table'
-        
     )
 }}
 
 with base as(
-    select distinct
+    select 
         customer_id,
         customer_unique_id,
         customer_zip_code_prefix,
@@ -14,10 +13,37 @@ with base as(
         customer_state
     from {{ ref('stg_customers') }}
 ), 
-    customers as(
-        select *
-        from base
+    customer_profile as(
+    select 
+        customer_unique_id,
+        gender,
+        birth_date,
+        loyalty_tier,
+        marketing_opt_in,
+        acquisition_channel
+    from {{ ref('stg_customer_profile')}}
+    ),
+
+    customers_enriched as(
+        select
+            b.customer_id,
+            b.customer_unique_id,
+
+            cp.gender,
+            cp.birth_date,
+
+            cp.loyalty_tier,
+            cp.marketing_opt_in,
+            cp.acquisition_channel,
+
+            b.customer_zip_code_prefix,
+            b.customer_city,
+            b.customer_state
+
+    from base b
+    left join customer_profile cp
+        using(customer_unique_id)
     )
 
 select *
-from customers
+from customers_enriched
